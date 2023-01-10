d6859aa389216f6f881586c1fff542040e422393
koltvictor <kolts.email@gmail.com>
2023-01-10 10:13:27 -0800

    added home nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import Search from "./components/Search";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
+      <Nav>
+        <GiKnifeFork />
+        <Logo to={"/"}>Recipes</Logo>
+      </Nav>
       <Search />
       <Category />
       <Pages />
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: Roboto, sans-serif;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
+
 export default App;
